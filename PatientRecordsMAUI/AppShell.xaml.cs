41e0473dba60692cad71eff06e8f13c9d15dba26
Trevoga <Trevoga@DESKTOP-ODLVMOO>
2026-04-09 08:28:52 +0200

    Add project files.

---
+++ PatientRecordsMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PatientRecordsMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PatientRecordsMAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PatientRecordsMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
